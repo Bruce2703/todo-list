8d0de62732367a63fdec4c1e96758f085c617f98
Bruce <68804621+Bruce2703@users.noreply.github.com>
2024-09-23 17:49:43 +0100

    This is a Todo list

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles1.css'
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <p>
+                Copyright @2024
+            </p>
+
+        </div>
+    )
+}
+export default Footer
---
+++ src/notes.js
@@ -0,0 +1,25 @@
+
+const notes= [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "Q. how many programmers does it tkake to change a light bulb? it's ahardware problem"
+    },
+    {
+        key: 2,
+        title: "Loop",
+        content: "Q. How to keep a programmer in shower forever, Show him the shampoo bottle instruction: Lather rinse repeat"
+
+    },
+    {
+        key: 3,
+        title: "Arrays",
+        content:"Q. Why dis the progerammer quit his job? A. Becasue he did'nt get arrays"
+    },
+    {
+        key: 4,
+        title: "Hardware vs Software",
+        content: "Q. What is the different between Hardware And Software? You can hit a hardware with a hammer, but you can onlu curse your software"    
+    }
+];
+export default notes
